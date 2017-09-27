c585bc66a3b53925fb71bef102c8f2de6f52a7ec
keith(mac) <ka33145@googlemail.com>
2017-09-27 12:50:55 +0100

    Added buzz test

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,8 +1,12 @@
 describe('FizzBuzz', function () {          // similar to RSpec 'describe' it sets up the test for the name of the function FizzBuzz
 
   it('prints fizz if the number is divisible by 3', function() {  // name of our first test, testing if the programme will output fizz if the number is divisible by 3
-    var x = new FizzBuzz();  // creating a new FizzBuzz instance (object)
-    expect(x.play(3)).toEqual('fizz');  // test to see if it works, play is a method/function that FizzBuzz has
+    var fizzbuzz = new FizzBuzz();  // creating a new FizzBuzz instance (object)
+    expect(fizzbuzz.play(3)).toEqual('fizz');  // test to see if it works, play is a method/function that FizzBuzz has
+  });
+  it('return buzz if number divisible by 5', function() {  // name of our first test, testing if the programme will output fizz if the number is divisible by 3
+    var fizzbuzz = new FizzBuzz();  // creating a new FizzBuzz instance (object)
+    expect(fizzbuzz.play(5)).toEqual('buzz');  // test to see if it works, play is a method/function that FizzBuzz has
   });
 
 
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,6 +1,6 @@
 function FizzBuzz() {}
 
 FizzBuzz.prototype.play = function (number) {
-  if (number % 3 === 0)
-  { return 'fizz'; }
+  if (number % 5 === 0) { return 'buzz'; }
+  else if (number % 3 === 0) { return 'fizz'; }
 }
