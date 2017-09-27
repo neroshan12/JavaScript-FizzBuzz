4a54927e13d1f5f901c2ce9f671e0965a9630016
Nero Siva <neroshan12@gmail.com>
2017-09-27 12:55:20 +0100

    Add fizzbuzz test and code

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,5 +1,9 @@
 describe('FizzBuzz', function () {          // similar to RSpec 'describe' it sets up the test for the name of the function FizzBuzz
 
+  it('return fizzbuzz if number divisible by 15', function() {  // name of our first test, testing if the programme will output fizz if the number is divisible by 3
+    var fizzbuzz = new FizzBuzz();  // creating a new FizzBuzz instance (object)
+    expect(fizzbuzz.play(15)).toEqual('fizzbuzz');  // test to see if it works, play is a method/function that FizzBuzz has
+  });
   it('prints fizz if the number is divisible by 3', function() {  // name of our first test, testing if the programme will output fizz if the number is divisible by 3
     var fizzbuzz = new FizzBuzz();  // creating a new FizzBuzz instance (object)
     expect(fizzbuzz.play(3)).toEqual('fizz');  // test to see if it works, play is a method/function that FizzBuzz has
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,6 +1,7 @@
 function FizzBuzz() {}
 
 FizzBuzz.prototype.play = function (number) {
-  if (number % 5 === 0) { return 'buzz'; }
+  if (number % 15 === 0) { return 'fizzbuzz'; }
+  else if (number % 5 === 0) { return 'buzz'; }
   else if (number % 3 === 0) { return 'fizz'; }
 }
